c1da9c49945fd406c0fc52ca5d528fe011ac95d3
Philipp Rottner <info@philipprottner.de>
2018-07-19 22:28:11 +0200

    start to implement containers actions, structure

--- src/app.js
+++ src/app.js
@@ -1,20 +1,58 @@
 import { h, app } from 'hyperapp'
+import recipes from './mocks/recipes'
 
 const state = {
-    count: 0
+    isLoading: true,
+    recipeFocused: {},
+    recipes: []
 }
 
+// TODO: HELPER FUNC
+const hash = (val) =>
+    val.split('').reduce((a, b) => {
+        a = (a << 5) - a + b.charCodeAt(0)
+        return a & a
+    }, 0)
+
+// TODO: HELPER FUNC
+const recipeMap = (recipes) => {
+    const recipeMap = new Map()
+    recipes.forEach((recipe) => {
+        recipeMap.set(hash(recipe.title), recipe)
+    })
+    return recipeMap
+}
+
+// TODO: merge actions from component's actions + global actions
 const actions = {
-    down: () => (state) => ({ count: state.count - 1 }),
-    up: () => (state) => ({ count: state.count + 1 })
+    // TODO: MAKE ASYNC FETCH & HAVE UI FOR EMPTY RECIPES
+    loadRecipes: () => () => ({ recipes: recipeMap(recipes), isLoading: false }),
+    focusRecipe: () => (recipeFocused) => ({ recipeFocused })
 }
 
-const view = (state, actions) => (
-    <main>
-        <h1>{state.count}</h1>
-        <button onclick={actions.down}>-</button>
-        <button onclick={actions.up}>+</button>
-    </main>
-)
+// TODO: show recipe details when a recipe is selected
+const Recipe = null
+
+// TODO: RecipeSelector shows loading indicator / no recipe status
+// TODO: RecipeSelector shows title, tags in a navigation bar / screen
+// TODO: RecipeSelector includes search bar, filters
+const RecipeSelector = null
+
+const view = (state, actions) => {
+    const { isLoading, recipeFocused, recipes } = state
+    const { loadRecipes, focusRecipe } = actions
+
+    return (
+        <main oncreate={loadRecipes}>
+            <RecipeSelector
+                isLoading={isLoading}
+                recipeFocused={recipeFocused}
+                recipes={recipes}
+                focusRecipe={focusRecipe}
+            />
+            {recipeFocused && <Recipe recipe={recipeFocused} />}
+        </main>
+    )
+}
 
 app(state, actions, view, document.body)
---
+++ src/mocks/recipes.js
@@ -0,0 +1,16 @@
+export const recipes = [
+    {
+        title: 'lava cake',
+        ingredients: ['chocolate', 'eggs', 'sugar'],
+        steps: ['be hungry', 'have ingredients', 'have a working recipe app'],
+        tags: ['desert', 'not lactose free', 'extremely yummie']
+    },
+    {
+        title: 'Müsli',
+        ingredients: ['nuts', 'honey', 'other stuff'],
+        steps: ['have no more Müsli', 'have a clean oven'],
+        tags: ["don't start a day without it", 'nearly healthy']
+    }
+]
+
+export default recipes
