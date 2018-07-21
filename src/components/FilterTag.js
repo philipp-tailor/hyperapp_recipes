d6d82870afe09e1f9de47ac610dee778d0dd4746
Philipp Rottner <info@philipprottner.de>
2018-07-21 20:23:14 +0200

    display recipe tag in recipe list

---
+++ src/components/FilterTag.js
@@ -0,0 +1,3 @@
+import { h } from 'hyperapp'
+
+export const FilterTag = ({ tag }) => <span>{tag}</span>
--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -1,3 +1,9 @@
 import { h } from 'hyperapp'
+import { FilterTag } from './FilterTag'
 
-export const RecipeListItem = ({ recipe }) => <div>{recipe.title}</div>
+export const RecipeListItem = ({ recipe }) => (
+    <div>
+        {recipe.title}
+        {recipe.tags.map((tag) => <FilterTag tag={tag} />)}
+    </div>
+)
